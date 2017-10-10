6885e311cd2bc2960d1aa022f0957abac6b5c9ed
Arthaey <arthaey@gmail.com>
2017-10-10 07:57:55 -0700

    Triggered builds can skip running tests.

--- bin/trigger_badges_update.sh
+++ bin/trigger_badges_update.sh
@@ -1,12 +1,23 @@
-# Add this to the .travis.yml of the OTHER project:
+# THIS SHOULD BE CALLED BY YOUR OTHER PROJECT WHOSE BADGE NEEDS UPDATING.
+#
+# Add the following to the .travis.yml of the OTHER project:
 #
 # after_script:
 #   - curl -o trigger.sh https://raw.githubusercontent.com/Arthaey/travis-build-history-badges/master/bin/trigger_badges_updates.sh
 #   - sh trigger.sh
+#
+# Be sure to define the $TRAVIS_ACCESS_TOKEN env var for the OTHER build.
 
 body='{
 "request": {
-"branch":"master"
+  "branch":"master",
+  "message":"Triggered by commit to $TRAVIS_REPO_SLUG.",
+  "config": {
+    "env": {
+      "RUN_TESTS": 0,
+      "UPDATE_BADGES": 1
+    }
+  }
 }}'
 
 curl -s -X POST \
