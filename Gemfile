70c0de5c51d7864acc5e36da5540f194daac1c8f
Arthaey <arthaey@gmail.com>
2017-10-09 22:50:05 -0700

    Upload code coverage to Coveralls.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'net-scp'
 gem 'travis'
 
 group :test do
+  gem 'coveralls', require: false
   gem 'minitest'
   gem 'rake'
   gem 'simplecov'
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,9 @@
 require "simplecov"
+require "coveralls"
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter,
+])
 SimpleCov.start do
   add_filter "lib/spark_pr.rb"
 end
